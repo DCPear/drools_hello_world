45b44502ae3211045b1a9247a13c5c260cd82e7f
Deepa Perera (DCPear) <dcpeartree@gmail.com>
2022-02-23 09:08:22 +1300

    logger and scanner class

--- src/main/java/com/dcpear/main/DroolsTest.java
+++ src/main/java/com/dcpear/main/DroolsTest.java
@@ -1,4 +1,8 @@
 package com.dcpear.main;
+/**
+ * Drools project for a jewellery shop to give discount to the type of jewellery.
+ * Rules for the dicount calculations are written in the rules.drl file
+ */
 
 import com.dcpear.model.Product;
 import org.kie.api.KieServices;
@@ -6,17 +10,22 @@ import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.rule.FactHandle;
 
+import java.util.Scanner;
+
 public class DroolsTest {
     public static void main(String[] args) {
         try {
             KieServices ks = KieServices.Factory.get();
             KieContainer kContainer = ks.getKieClasspathContainer();
-//Get the session named kseesion-rule that we defined in kmodule.xml above.
-//Also by default the session returned is always stateful.
+            //Get the session named kseesion-rule that we defined in kmodule.xml above.
+            //Also by default the session returned is always stateful.
             KieSession kSession = kContainer.newKieSession("ksession-rule");
 
             Product product = new Product();
-            product.setType("diamond");
+            Scanner sc= new Scanner(System.in);
+            System.out.print("Enter jewellery type - ");
+            String str= sc.nextLine();
+            product.setType(str);
 
 
             FactHandle fact1 = kSession.insert(product);
